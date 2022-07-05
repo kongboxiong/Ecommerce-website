4d0b479d08d2936444404b4fa68c0d91916b4afb
boxiongkong <106040875+kongboxiong@users.noreply.github.com>
2022-07-05 01:29:20 -0400

    Add files via upload

---
+++ angular-ecommerce-frontend/src/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+
+    oidc: {
+        clientId: '0oa5kkifti0MVIDGW5d7',
+        issuer: 'https://dev-43064368.okta.com/oauth2/default',
+        redirectUri: 'https://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+
+}
